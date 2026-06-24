065f727395fa76592744bece57fafa92077ba00f
afhuertass <afhuertass@unal.edu.co>
2026-06-24 16:38:47 +0300

    path

--- lib/personal_space/kepler_link.ex
+++ lib/personal_space/kepler_link.ex
@@ -3,7 +3,7 @@ defmodule PersonalSpace.KeplerLink do
 
   def generate(hours \\ 24) do
     data_url =
-      "http://dg06h8difqv1i4716lwtfj69.89.167.124.166.sslip.io/api/flights/geojson?hours=#{hours}"
+      "http://dg06h8difqv1i4716lwtfj69.89.167.124.166.sslip.io/api/flights/geojson"
 
     encoded = URI.encode(data_url)
     "#{@base_url}?mapUrl=#{encoded}"
